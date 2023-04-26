8b58746931c084af582ea01ccfd4a8b7994151b0
Ayushman_Choudhuri <ayushc205@gmail.com>
2023-04-26 08:45:52 +0200

    code to display track replaced with tracker.display

--- main.py
+++ main.py
@@ -27,6 +27,10 @@ tracker = DeepSortTracker()
 
 track_history = {}    # Define a dictionary to store the previous center locations for each track ID
 
+# Define the output video codec and filename
+#fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+#out = cv2.VideoWriter('output.mp4', fourcc, 30.0, (int(cap.get(3)),int(cap.get(4))))
+
 while cap.isOpened():
 
     success, img = cap.read() # Read the image frame from data source 
@@ -34,40 +38,15 @@ while cap.isOpened():
     start_time = time.perf_counter()    #Start Timer - needed to calculate FPS
     
     # Object Detection
-    results = object_detector.score_frame(img)  # run the yolo v5 object detector 
+    results = object_detector.run_yolo(img)  # run the yolo v5 object detector 
     detections , num_objects= object_detector.extract_detections(results, img, height=img.shape[0], width=img.shape[1]) # Plot the bounding boxes and extract detections (needed for DeepSORT) and number of relavent objects detected
 
-
     # Object Tracking
-    tracks = tracker.object_tracker.update_tracks(detections, frame=img)
-
-    for track in tracks:
-        if not track.is_confirmed():
-            continue
-        track_id = track.track_id
-
-         # Retrieve the current track location and bounding box
-        location = track.to_tlbr()
-        bbox = location[:4].astype(int)
-        bbox_center = ((bbox[0] + bbox[2]) // 2, (bbox[1] + bbox[3]) // 2)
-         # Retrieve the previous center location, if available
-        prev_center = track_history.get(track_id)
-
-        # Draw the track line, if there is a previous center location
-        if prev_center is not None:
-            cv2.line(img, prev_center, bbox_center, (255, 0, 0), 2)
-            
-
-        cv2.rectangle(img,(int(bbox[0]), int(bbox[1])),(int(bbox[2]), int(bbox[3])),(0,0,255),2)
-        cv2.putText(img, "ID: " + str(track_id), (int(bbox[0]), int(bbox[1] - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-
-         # Store the updated history for this track ID
-        track_history[track_id] = bbox_center
-        
-    
-    end_time = time.perf_counter()
+    tracks_current = tracker.object_tracker.update_tracks(detections, frame=img)
+    tracker.display_track(track_history , tracks_current , img)
     
     # FPS Calculation
+    end_time = time.perf_counter()
     total_time = end_time - start_time
     fps = 1 / total_time
 
