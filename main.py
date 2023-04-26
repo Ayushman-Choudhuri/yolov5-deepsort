cf0f32c2cc61bbe8de3fc03b65c72a1069751db5
Ayushman_Choudhuri <ayushc205@gmail.com>
2023-04-26 13:24:41 +0200

    code structure corrected

--- main.py
+++ main.py
@@ -4,32 +4,28 @@ import os
 import sys
 import yaml
 
+from src.detector import YOLOv5Detector
+from src.tracker import DeepSortTracker
+from src.dataloader import cap
+
+# Parameters from config.yml file
 with open('config.yml' , 'r') as f:
     config =yaml.safe_load(f)['yolov5_deepsort']['main']
 
 # Add the src directory to the module search path
 sys.path.append(os.path.abspath('src'))
 
-from src.detector import YOLOv5Detector
-from src.tracker import DeepSortTracker
-from src.dataloader import cap
+# Get YOLO Model Parameter
+YOLO_MODEL_NAME = config['model_name']
 
-YOLO_MODEL_NAME = 'yolov5n'
-
-# Image Display Parameters
+# Visualization Parameters
 DISP_FPS = config['disp_fps'] 
 DISP_OBJ_COUNT = config['disp_obj_count']
-DISP_TRACKS = config['disp_tracks'] 
-DISP_OBJ_TRACK_BOX = config['disp_obj_track_box']
 
 object_detector = YOLOv5Detector(model_name=YOLO_MODEL_NAME)
 tracker = DeepSortTracker()
 
-track_history = {}    # Define a dictionary to store the previous center locations for each track ID
-
-# Define the output video codec and filename
-#fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-#out = cv2.VideoWriter('output.mp4', fourcc, 30.0, (int(cap.get(3)),int(cap.get(4))))
+track_history = {}    # Define a empty dictionary to store the previous center locations for each track ID
 
 while cap.isOpened():
 
@@ -42,7 +38,7 @@ while cap.isOpened():
     detections , num_objects= object_detector.extract_detections(results, img, height=img.shape[0], width=img.shape[1]) # Plot the bounding boxes and extract detections (needed for DeepSORT) and number of relavent objects detected
 
     # Object Tracking
-    tracks_current = tracker.object_tracker.update_tracks(detections, frame=img)
+    tracks_current = tracker.object_tracker.update_tracks(detections, frame=img)#
     tracker.display_track(track_history , tracks_current , img)
     
     # FPS Calculation
@@ -51,7 +47,7 @@ while cap.isOpened():
     fps = 1 / total_time
 
 
-    # Descriptions on the image 
+    # Descriptions on the output visualization
     cv2.putText(img, f'FPS: {int(fps)}', (20,40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1)
     cv2.putText(img, f'MODEL: {YOLO_MODEL_NAME}', (20,60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1)
     cv2.putText(img, f'TRACKED CLASS: {object_detector.tracked_class}', (20,80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1)
